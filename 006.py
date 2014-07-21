fb6b63229b3c0838d7c9b58e8a6dd743e9914678
hkurokawa <hiroshi.kurokawa@gunosy.com>
2014-07-21 17:18:40 +0900

    Solved 006

--- 006.py
+++ 006.py
@@ -15,4 +15,18 @@ Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
 """
 
+def enum_pairs(ls):
+    pairs = []
+    n = len(ls)
+    for i in range(n):
+        for j in range(i+1, n):
+            pairs.append((ls[i], ls[j]))
+    return pairs
 
+if __name__ == "__main__":
+    n = 100
+    terms = range(1, n+1)
+    diff = 0
+    for a, b in enum_pairs(terms):
+        diff += a * b * 2
+    print diff
