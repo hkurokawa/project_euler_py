6e60824675c695c8358945197d47fb0c5c65def1
hkurokawa <hiroshi.kurokawa@gunosy.com>
2014-07-21 16:55:18 +0900

    Improve the solution for 005 to use GCD and LCM.

--- 005.py
+++ 005.py
@@ -9,64 +9,23 @@ What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
 
-def is_prime(n, prime_nums_list):
-    """Return True if the given number is not divisible
-    by any number in the list. Return False otherwise."""
-    for v in prime_nums_list:
-        # If the next prime number is greater than n / 2,
-        # it means that n is prime.
-        if v > n / 2:
-            return True
-        if n % v == 0:
-            return False
-    return True
+def gcd(a, b):
+    if a < 0 or b < 0:
+        raise ValueError('Invalid arguments. Both the values must be positive integer', a, b)
+    elif a < b:
+        return gcd(b, a)
+    elif b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
-def gen_prime():
-    """Return a prime number generator."""
-    prime_nums_list = []
-    num = 2
-    while True:
-        if is_prime(num, prime_nums_list):
-            prime_nums_list.append(num)
-            yield num
-        num = num + 1
-
-def list_primes(max_num):
-    primes = gen_prime()
-    ls = []
-    for i in primes:
-        if i > max_num:
-            break
-        ls.append(i)
-    return ls
-
-def prime_factorise(n, prime_nums_list):
-    if n == 1:
-        return dict()
-    for p in prime_nums_list:
-        if n % p == 0:
-            factors = prime_factorise(n / p, prime_nums_list)
-            if not p in factors:
-                factors[p] = 1
-            else:
-                factors[p] = factors[p] + 1
-            return factors
-    raise ValueError('The given number is not divisible with the given prime numbers list', n, prime_nums_list)
+def lcm(a, b):
+    gcd_val = gcd(a, b)
+    return a * b / gcd_val
 
 if __name__ == "__main__":
     n = 20
-    primes = list_primes(n)
-    sum_factors = dict()
-    for i in range(2, n, 1):
-        factors = prime_factorise(i, primes)
-        for k, v in factors.items():
-            if not k in sum_factors:
-                sum_factors[k] = 1
-            elif sum_factors[k] < v:
-                sum_factors[k] = v
-    ans = 1
-    for v, f in sum_factors.items():
-        while f > 0:
-            ans = ans * v
-            f = f - 1
+    ans = 2520
+    for v in range(11, n, 1):
+        ans = lcm(v, ans)
     print ans
