dc71b7ac03cc4a4215eab5d42a761d8f20b82fea
hkurokawa <hiroshi.kurokawa@gunosy.com>
2014-07-24 03:12:13 +0900

    Make 001 more Pythonic.

--- 001.py
+++ 001.py
@@ -9,8 +9,4 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 if __name__ == "__main__":
-    sum = 0
-    for n in range(1000):
-        if n % 3 == 0 or n % 5 == 0:
-            sum = sum + n
-    print sum
+    print reduce(lambda x, y: x + y, [n for n in range(1, 1000) if n % 3 == 0 or n % 5 == 0])
